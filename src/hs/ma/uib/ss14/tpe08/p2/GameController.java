f07df0e2ce4ff6ee61159c7d86fefd837c9e57a0
mustang2015 <giangpham92@gmail.com>
2014-04-15 16:01:29 +0200

    3. Commit

---
+++ src/hs/ma/uib/ss14/tpe08/p2/GameController.java
@@ -0,0 +1,13 @@
+package hs.ma.uib.ss14.tpe08.p2;
+
+public class GameController {
+
+	public void runGame() {
+		GameController game = new GameController();
+		GameViewer.printGame(game);
+
+	}
+
+
+
+}
---
+++ src/hs/ma/uib/ss14/tpe08/p2/GameViewer.java
@@ -0,0 +1,7 @@
+package hs.ma.uib.ss14.tpe08.p2;
+
+public class GameViewer {
+	public static String printGame(GameController game){
+		return "test";
+	}
+}
