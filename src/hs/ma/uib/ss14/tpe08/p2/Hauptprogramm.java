1a4627cd8e9087189afe929a5e4f9931b5dd2991
kingj_000 <kingj_000@134.155.163.71>
2014-04-22 18:33:05 +0200

    // 3 Kaempfe erzeugt, damit Angriff getestet wird, Angriff mit Elemente die Stimmen (also Fallunterscheidung ob damage doppelt gezaehlt, oder einfach gezaehlt wird)

--- src/hs/ma/uib/ss14/tpe08/p2/Hauptprogramm.java
+++ src/hs/ma/uib/ss14/tpe08/p2/Hauptprogramm.java
@@ -9,14 +9,58 @@ public class Hauptprogramm {
 
 	public static void main(String[] args) {
 		Erzmagier giang = Erzmagier.getInstance();
+		Daemonenjaeger sovann = Daemonenjaeger.getInstance();
 
-		Daemonenjaeger hunar = Daemonenjaeger.getInstance();
+		Ork fisch = new Ork();
+		Mensch blau = new Mensch();
 		Nachtelf onur = new Nachtelf();
 		Untote burhan = new Untote();
+
+		System.out
+				.println("_______________________erster_________________________");
 		System.out.println(giang);
-		System.out.println(hunar);
-		System.out.println(giang.attack(hunar));
-		System.out.println(hunar.getHP());
+		System.out
+				.println("______________________________________________________");
+		System.out.println(sovann);
+		System.out
+				.println("______________________________________________________");
+		System.out.println(giang.attack(sovann));
+		System.out.println(sovann.getHP());
+		System.out
+				.println("______________________________________________________");
+		System.out.println();
+		System.out.println();
+		System.out
+				.println("______________________zweiter_________________________");
+
+		System.out.println(fisch);
+		System.out
+				.println("______________________________________________________");
+		System.out.println(burhan);
+		System.out
+				.println("______________________________________________________");
+		System.out.println(burhan.attack(fisch));
+		System.out.println(fisch.getHP());
+		System.out
+				.println("______________________________________________________");
+
+		System.out.println();
+		System.out.println();
+
+		System.out
+				.println("______________________dritter_________________________");
+
+		System.out.println(blau);
+		System.out
+				.println("______________________________________________________");
+		System.out.println(burhan);
+		System.out
+				.println("______________________________________________________");
+		System.out.println(blau.attack(burhan));
+		System.out.println(burhan.getHP());
+		System.out
+				.println("______________________________________________________");
+
 	}
 
 }
--- src/hs/ma/uib/ss14/tpe08/p2/WesenFactory.java
+++ src/hs/ma/uib/ss14/tpe08/p2/WesenFactory.java
@@ -1,20 +1,18 @@
 package hs.ma.uib.ss14.tpe08.p2;
 
 public class WesenFactory {
-/*
+
 	public static void kaufeWesen(Squad s) {
 		Rasse rasse = null;
 		switch (rasse) {
 		case Ork:
-			rasse = new Ork();
-			s.hinzufuegen(rasse);
 			break;
-		case Farseer:
-			rasse = Farseer.getInstance();
-		default:
+		case Untote:
+			break;
+		case Mensch:
+			break;
+		case Nachtelf:
 			break;
 		}
-
 	}
-	*/
 }
