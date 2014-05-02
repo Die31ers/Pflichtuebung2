d5959011d3ca003f8b3e3679752364afa1c74b4e
mustang2015 <giangpham92@gmail.com>
2014-05-02 22:26:38 +0200

    Endspurt. JUnit und JavaDocs rein. 00:36

---
+++ src/hs/ma/uib/ss14/tpe08/p2/Test/Teste.java
@@ -0,0 +1,121 @@
+package hs.ma.uib.ss14.tpe08.p2.Test;
+
+import static org.junit.Assert.*;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Daemonenjaeger;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Erzmagier;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Farseer;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Lich;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Nachtelf;
+import hs.ma.uib.ss14.tpe08.p2.Wesen.Ork;
+import hs.ma.uib.ss14.tpe08.p2.WesenErzeugen.Rasse;
+import hs.ma.uib.ss14.tpe08.p2.WesenErzeugen.Squad;
+
+import org.junit.Test;
+
+public class Teste {
+
+	Squad s1 = new Squad("Daemonenjaeger Gruppe", Rasse.Daemonenjaeger, 1000,
+			Rasse.Mensch, 1000);
+	Squad s2 = new Squad("Lich Gruppe", Rasse.Lich, 1200, Rasse.Nachtelf, 800);
+
+	@Test
+	public void testeNamen() {
+		assertEquals("Daemonenjaeger Gruppe", s1.toString());
+	}
+
+	@Test
+	public void testeNamen1() {
+		assertEquals("Lich Gruppe", s2.toString());
+
+	}
+
+	@Test
+	public void testeEnum() {
+		assertEquals(70, Rasse.Untote.getKosten());
+	}
+
+	@Test
+	public void testeEnum1() {
+		assertEquals(290, Rasse.Daemonenjaeger.getKosten());
+	}
+
+	@Test
+	public void testeAngriff() {
+		assertEquals(8000.0,
+				Erzmagier.getInstance().attack(Daemonenjaeger.getInstance()), 0);
+	}
+
+	@Test
+	public void testeAngriff1() {
+		assertEquals(132, new Ork().attack(new Nachtelf()), 0);
+	}
+
+	@Test
+	public void TestSingelton() {
+		assertTrue(Daemonenjaeger.getInstance().equals(
+				Daemonenjaeger.getInstance()));
+	}
+
+	@Test
+	public void testeVerwesung() {
+		assertEquals(276, Lich.getInstance().getHP(), 0);
+		Squad s5 = new Squad("l", Rasse.Nachtelf, 2000);
+		Lich.getInstance().verwesung(s5);
+		assertEquals(114.4, s5.team.get(0).getHP(), 0);
+		assertEquals(13, s5.getAnzahl(), 0);
+		assertEquals(367, Lich.getInstance().getHP(), 0);
+	}
+
+	@Test
+	public void testeGoldschuss() {
+		Squad s3 = new Squad("Nase", Rasse.Mensch, 1000, Rasse.Ork, 1000);
+		Daemonenjaeger.getInstance().goldschuss(s3);
+		assertEquals(126.5, s3.team.get(1).getHP(), 0);
+	}
+
+	@Test
+	public void testeDoppelschlag() {
+		Squad s4 = new Squad("Test", Rasse.Ork, 1000, Rasse.Untote, 1000);
+		assertEquals(19, s4.team.size(), 0);
+		Farseer.getInstance().doppelschlag(s4);
+		assertEquals(17, s4.team.size(), 0);
+	}
+
+	@Test
+	public void teste1Absorption() {
+		Erzmagier.getInstance().absorption();
+		Farseer.getInstance().attack(Erzmagier.getInstance());
+		assertEquals(700, Erzmagier.getInstance().getHP(), 0);
+	}
+
+	 /*
+	  * Feuer(Erzmagier )stärker Luft(Dämonenjäger)
+	  * Wasser(Lich)stärker Feuer (Erzmagier )
+	  * Erde(Farseer) stärker Wasser(Lich)
+	  * Luft(Dämonenjäger) stärker Erde(Farseer)
+	  */
+
+	@Test
+	public void testeDoppelterSchaden1() {
+		assertEquals(8000,
+				Erzmagier.getInstance().attack(Daemonenjaeger.getInstance()), 0);
+	}
+
+	@Test
+	public void testeDoppelterSchaden2() {
+		assertEquals(316.8, Farseer.getInstance().attack(Lich.getInstance()), 0);
+	}
+
+	@Test
+	public void testeDoppelterSchaden3() {
+		assertEquals(783,
+				Daemonenjaeger.getInstance().attack(Farseer.getInstance()), 0);
+	}
+
+	@Test
+	public void testeDoppelterSchaden4() {
+		assertEquals(235.52,
+				Lich.getInstance().attack(Erzmagier.getInstance()), 0.0000001);
+
+	}
+}
--- src/hs/ma/uib/ss14/tpe08/p2/WesenErzeugen/WesenFactory.java
+++ src/hs/ma/uib/ss14/tpe08/p2/WesenErzeugen/WesenFactory.java
@@ -22,15 +22,6 @@ import hs.ma.uib.ss14.tpe08.p2.Wesen.Wesen;
  * 
  */
 public class WesenFactory {
-	public static void erstelleWesen(Squad s, Rasse rasse, int investition,
-			Wesen w) {
-		int erg = investition;
-		do {
-			s.hinzufuegen(w);
-			erg = erg - rasse.getKosten();
-
-		} while (erg - rasse.getKosten() > 0);
-	}
 
 	public static void kaufeWesen(Squad s, Rasse rasse, int investition) {
 		int erg = investition;
@@ -40,10 +31,9 @@ public class WesenFactory {
 				s.hinzufuegen(Farseer.getInstance());
 				erg = erg - Rasse.Farseer.getKosten();
 			}
-			while (erg - rasse.getKosten() > 0){
+			while (erg - rasse.getKosten() > 0) {
 				s.hinzufuegen(new Ork());
 				erg = erg - rasse.getKosten();
-			// erstelleWesen(s, Rasse.Ork, erg, new Ork());
 			}
 			break;
 		case Untote:
@@ -51,21 +41,19 @@ public class WesenFactory {
 				s.hinzufuegen(Lich.getInstance());
 				erg = erg - Rasse.Lich.getKosten();
 			}
-			while (erg - rasse.getKosten() > 0){
+			while (erg - rasse.getKosten() > 0) {
 				s.hinzufuegen(new Untote());
 				erg = erg - rasse.getKosten();
-			// erstelleWesen(s, Rasse.Untote, erg, new Untote());
 			}
-				break;
+			break;
 		case Mensch:
 			if (Erzmagier.istVorhanden() == false) {
 				s.hinzufuegen(Erzmagier.getInstance());
 				erg = erg - Rasse.Erzmagier.getKosten();
 			}
-			while (erg - rasse.getKosten() > 0){
+			while (erg - rasse.getKosten() > 0) {
 				s.hinzufuegen(new Mensch());
 				erg = erg - rasse.getKosten();
-			// erstelleWesen(s, Rasse.Mensch, erg, new Mensch());
 			}
 			break;
 		case Nachtelf:
@@ -73,11 +61,9 @@ public class WesenFactory {
 				s.hinzufuegen(Daemonenjaeger.getInstance());
 				erg = erg - Rasse.Daemonenjaeger.getKosten();
 			}
-			while (erg - rasse.getKosten() > 0){
+			while (erg - rasse.getKosten() > 0) {
 				s.hinzufuegen(new Nachtelf());
 				erg = erg - rasse.getKosten();
-			
-			// erstelleWesen(s, Rasse.Nachtelf, erg, new Nachtelf());
 			}
 			break;
 		}
